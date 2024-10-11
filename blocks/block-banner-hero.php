cf5ad06565dd8c7da304094344278912450aaa6a
Danylo <dakup@MacBook-Pro-Danylo.local>
2024-10-11 13:46:24 +0200

    Banner Hero and Buttons

--- blocks/block-banner-hero.php
+++ blocks/block-banner-hero.php
@@ -1,8 +1,50 @@
+<?php
+$title = get_field('title') ?? null;
+$content = get_field('content') ?? null;
+$button_1 = get_field('button_1') ?? null;
+$button_2 = get_field('button_2') ?? null;
+$bg_img = get_field('bg') ?? null;
+?>
+
 <section class="banner-hero">
-    <div class="font-title">
-        Test title
+    <div class="banner-hero__inner container">
+        <figure class="banner-hero__decoration">
+            <img src="<?php echo IMG . 'spinner-main-light.svg' ?>" alt="BR Druk">
+            <img src="<?php echo IMG . 'spinner-text-light.svg' ?>" alt="BR Druk">
+        </figure>
+        <?php if ($title): ?>
+            <h1 class="banner-hero__title font-title">
+                <?php echo $title ?>
+            </h1>
+        <?php endif; ?>
+        <?php if ($content): ?>
+            <div class="banner-hero__content text-content">
+                <?php echo $content ?>
+            </div>
+        <?php endif; ?>
+
+        <?php if ($button_1 || $button_2): ?>
+            <div class="banner-hero__buttons">
+                <?php if ($button_1): ?>
+                    <a href="<?php echo $button_1['url'] ?>" class="btn btn--dark" title="<?php echo $button_1['title'] ?>">
+                        <span>
+                            <?php echo $button_1['title'] ?>
+                        </span>
+                    </a>
+                <?php endif; ?>
+
+                <?php if ($button_2): ?>
+                    <a href="<?php echo $button_2['url'] ?>" class="btn btn--light" title="<?php echo $button_2['title'] ?>">
+                        <span>
+                            <?php echo $button_2['title'] ?>
+                        </span>
+                    </a>
+                <?php endif; ?>
+            </div>
+        <?php endif; ?>
     </div>
-    <div class="text-content">
-        <p>Test Text Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi illum nam, veniam officiis delectus debitis. Maiores quae libero saepe vitae labore quam illum fuga, magnam delectus assumenda odit, animi iste.</p>
-    </div>
+
+    <?php if ($bg_img) : ?>
+        <img src="<?php echo $bg_img ?>" alt="BR Druk Opole" class="banner-hero__bg">
+    <?php endif; ?>
 </section>
--- fields/block-banner-hero.php
+++ fields/block-banner-hero.php
@@ -1,42 +1,51 @@
 <?php
 
-function register_banner_fields() {
+function banner_hero_fields() {
+    $key = 'banner_hero_fields_';
     acf_add_local_field_group([
-        'key' => 'group_block_banner_hero',
+        'key' => $key,
         'title' => 'Banner',
         'fields' => [
             [
+                'key' => $key . 'title',
                 'name' => 'title',
                 'label' => 'Title',
                 'type' => 'text',
             ],
             [
+                'key' => $key . 'content',
                 'name' => 'content',
                 'label' => 'Content',
                 'type' => 'wysiwyg',
             ],
             [
+                'key' => $key . 'bg',
                 'name' => 'bg',
                 'label' => 'Background Image',
                 'type' => 'image',
                 'return_format' => 'url',
+                'wrapper' => [
+                    'width' => '33%',
+                ]
             ],
             [
+                'key' => $key . 'button_1',
                 'name' => 'button_1',
                 'label' => 'Button 1',
                 'type' => 'link',
                 'return_format' => 'array',
                 'wrapper' => [
-                    'width' => '50%',
+                    'width' => '33%',
                 ]
             ],
             [
+                'key' => $key . 'button_2',
                 'name' => 'button_2',
                 'label' => 'Button 2',
                 'type' => 'link',
                 'return_format' => 'array',
                 'wrapper' => [
-                    'width' => '50%',
+                    'width' => '33%',
                 ]
             ],
         ],
@@ -51,4 +60,4 @@ function register_banner_fields() {
         ],
     ]);
 }
-add_action('acf/init', 'register_banner_fields');
+add_action('acf/init', 'banner_hero_fields');
