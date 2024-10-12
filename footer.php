db72d0d1bc2a75c11aed2c6138aff7de77093bd4
Danylo <dakup@MacBook-Pro-Danylo.local>
2024-10-12 14:46:45 +0200

    RWD

--- footer.php
+++ footer.php
@@ -1,24 +1,31 @@
 </main>
 
 <footer class="footer">
-    <div class="container">
-        <div class="footer__left">
-            <span class="footer__title">
-                Kontakt
-            </span>
-            <div class="footer__text text-content">
-                <p>Jakiś tekst</p>
+    <div class="footer__inner ">
+        <div class="container">
+            <div class="footer__logo-wrapper">
+                <img src="<?php echo IMG . 'logo-light.svg' ?>" alt="BR Druk" class="footer__logo">
+            </div>
+            <div class="footer__left">
+                <span class="footer__title font-title">
+                    Kontakt
+                </span>
+                <div class="footer__text text-content">
+                    <p>Jakiś tekst</p>
+                </div>
+            </div>
+            <div class="footer__right">
+                <span class="footer__title font-title">
+                    Godziny otwarcia
+                </span>
+                <div class="footer__text text-content">
+                    <p>Jakiś tekst</p>
+                </div>
             </div>
         </div>
-        <div class="footer__right">
-            <span class="footer__title">
-                Godziny otwarcia
-            </span>
-            <div class="footer__text text-content">
-                <p>Jakiś tekst</p>
-            </div>
-        </div>
-        <div class="footer__copy">
+    </div>
+    <div class="footer__copy">
+        <div class="container">
             BR Druk 2024
         </div>
     </div>
